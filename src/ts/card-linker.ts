deaf46209abd736659f2cbdd4c5506939f008cfc
Tetra <me@tetra.cool>
2021-12-29 16:50:10 -0500

    only listen for L mouseup

--- src/ts/card-linker.ts
+++ src/ts/card-linker.ts
@@ -1,7 +1,8 @@
 const hoverElements = document.querySelectorAll("[data-service-name]");
 
 for (const el of hoverElements as any) {
-    el.addEventListener("mouseup", (e: Event) => {
+    el.addEventListener("mouseup", (e: MouseEvent) => {
+        if (e.button !== 0) return;
         window.location.href = `${window.location.protocol}//${el.dataset.serviceName}.${window.location.hostname != "localhost" ? window.location.hostname : "home.tetra.cool"}`;
     });
 }

9b14a1b8248235d3b7ec2b22d3d0da4cf27de5ac
Tetra <me@tetra.cool>
2022-05-18 02:57:43 -0400

    lowercase greetings

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,21 +2,20 @@ import "../scss/style.scss";
 import App from "../svelte/App.svelte";
 
 const greetings = [
-  "welcome home",            // English
-  "bienvenido a casa",       // Spanish
-  "willkommen zuhause",      // German
-  "bienvenue à la maison",   // French
-  "benvenuto a casa",        // Italian
-  "welkom thuis",            // Dutch
-  "maligayang pagbabalik",   // Tagalog
-  "おかえりなさい",            // Japanese
-  "欢迎回家",                 // Chinese (simplified)
-  "歡迎回家",                 // Chinese (traditional)
-  "eve hoşgeldin",           // Turkish
-  "καλώς όρισες σπίτι",      // Greek
-  "Добро пожаловать домой",  // Russian
-  "Ласкаво просимо до дому", // Ukrainian
-  "witaj w domu"             // Polish
+  "welcome home", // English
+  "bienvenido a casa", // Spanish
+  "willkommen zuhause", // German
+  "bienvenue à la maison", // French
+  "benvenuto a casa", // Italian
+  "welkom thuis", // Dutch
+  "maligayang pagbabalik", // Tagalog
+  "おかえりなさい", // Japanese
+  "欢迎回家", // Chinese (simplified)
+  "歡迎回家", // Chinese (traditional)
+  "καλώς όρισες σπίτι", // Greek
+  "добро пожаловать домой", // Russian
+  "ласкаво просимо до дому", // Ukrainian
+  "witaj w domu" // Polish
 ];
 
 export default new App({

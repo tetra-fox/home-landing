5bc595438a08543c2cd924e7d45ee8e9fea5496d
tetra <github@tetra.cool>
2023-04-23 03:13:46 -0600

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 export default {
   async get(containers: string[] = []): Promise<StatusResponse> {
-    const res: Response = await fetch(`https://home.tetra.cool/api/${containers.join(",")}`);
+    const res: Response = await fetch(`https://dtw.tetra.cool/api/${containers.join(",")}`);
     if (res.status !== 200) throw new Error("Failed to get status");
 
     return await res.json();
